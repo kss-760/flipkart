17b4bf79b6d1cc711bf8421fc136260f12b87bd3
kishore <kishoreyuva50@gmail.com>
2020-02-17 18:20:44 +0530

    two files updated

--- babu.java
+++ babu.java
@@ -3,4 +3,9 @@ need to update
 and need to see the updated lines
 hi babu
 how is ur day dear
-
+$name
+date
+amma
+nanna
+tammudu
+akka chelli
--- hari.java
+++ hari.java
@@ -2,3 +2,7 @@ hi hari
 how r u dear
 and how ur friend
 how is your day dear
+yeah it was ok and going fine
+kishore
+kiran
+
